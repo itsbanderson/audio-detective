6edae6c242af3a2dfc90d61f7375fc25dbda6b9c
David Anderson <davidjosepha@gmail.com>
2013-08-31 07:29:19 -0500

    generate_transcode() function now requires absolute paths, generate_transcode.py call now resolves relative paths to absolute

--- generate_transcode.py
+++ generate_transcode.py
@@ -2,24 +2,34 @@ import sys, glob, os
 from subprocess import call
 
 def generate_transcode(indir, outdir, bitrate):
-    os.chdir(os.path.join(os.getcwd(), indir))
-    outpath = os.path.join(os.getcwd(), outdir)
-    call(["mkdir", os.path.join(outpath, "tmp")])
+    call(["mkdir", "-p", os.path.join(outdir, "tmp")])
+    tmpdir = os.path.join(outdir, "tmp")
 
-    tmpdir = os.path.join(outpath, "tmp")
+    os.chdir(indir)
     for track in glob.glob("*.flac"):
         outputfile = os.path.join(tmpdir, ".".join(track.split(".")[:-1]) + ".mp3")
         call(["ffmpeg", "-loglevel", "0", "-i", track, "-b", bitrate, "-y", outputfile]) 
 
     os.chdir(tmpdir)
     for track in glob.glob("*.mp3"):
-        outputfile = os.path.join(outpath, ".".join(track.split(".")[:-1]) + ".flac")
-        call(["ffmpeg", "-loglevel", "0", "-i", track, outputfile]) 
+        outputfile = os.path.join(outdir, ".".join(track.split(".")[:-1]) + ".flac")
+        #call(["ffmpeg", "-loglevel", "0", "-i", track, outputfile]) 
 
     call(["rm", "-r", tmpdir])
 
 if __name__ == "__main__":
-    indir = sys.argv[1]
-    outdir = sys.argv[2]
-    bitrate = sys.argv[3]
-    gen(indir, outdir, bitrate)
+    relindir = sys.argv[1]
+    reloutdir = sys.argv[2]
+    bit = sys.argv[3]
+    path = os.getcwd()
+
+    if bit == "all":
+        bitrates = ["128", "192", "256", "320"]
+        for bitrate in bitrates:
+            indir = os.path.join(path, relindir)
+            outdir = os.path.join(path, reloutdir, "flac", bitrate)
+            generate_transcode(indir, outdir, bitrate)
+    else:
+        indir = os.path.join(path, relindir)
+        outdir = os.path.join(path, reloutdir)
+        generate_transcode(indir, outdir, bit)
